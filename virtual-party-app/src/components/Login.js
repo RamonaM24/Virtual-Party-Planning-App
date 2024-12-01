5966fc2297623f1beed8193034d74199003327ae
Billie501 <132241423+Billie501@users.noreply.github.com>
2024-12-02 11:59:57 +1300

    Profile.js and Register.js created. Login.js updated

--- virtual-party-app/src/components/Login.js
+++ virtual-party-app/src/components/Login.js
@@ -6,9 +6,9 @@ const Login = () => {
         <div>
             <h1>Login Page</h1>
             <form>
-                        <input type='email' name='email' placeholder='Your Email Address' className='footer-input'>
+                        <input type='email' name='email' placeholder='Your Email Address' className='login-input'>
                         </input>
-                        <input type='password' name='password' placeholder='Your Password' className='footer-input'>
+                        <input type='password' name='password' placeholder='Your Password' className='login-input'>
                         </input>
                         <Button buttonStyle='btn--outline'>Subscribe</Button>
                     </form>
---
+++ virtual-party-app/src/components/Register.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Button } from './Button';
+
+const Register = () => {
+    return (
+        <div>
+            <h1>Registration Page</h1>
+            <form>
+                        <input type='name' name='name' placeholder='Your First Name' className='register-input'>
+                        </input>
+                        <input type='surname' name='surname' placeholder='Your Last Name' className='register-input'>
+                        </input>
+                        <input type='email' name='email' placeholder='Your Email Address' className='register-input'>
+                        </input>
+                        <input type='company' name='company' placeholder='Name of Company (if applicable)' className='register-input'>
+                        </input>
+                        <input type='dateofbirth' name='dateofbirth' placeholder='Date / Month / Year of Birth' className='register-input'>
+                        </input>
+                        <Button buttonStyle='btn--outline'>Submit</Button>
+                    </form>
+        </div>
+    );
+};
+
+export default Register;
