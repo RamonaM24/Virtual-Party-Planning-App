cd0585e0db567857df92e4ddbfbab9fb0a9d76e6
Ramona Matautia <wnr2328@autuni.ac.nz>
2024-12-10 03:51:00 +1300

    modified interface design on css and routes on js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,3 @@
-// import './App.css'; // Make sure this is at the top of your App.js file
-// import React from 'react';
-// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Corrected import for React Router
-// import EventsCreationForm from './Components/EventsCreationForm';  // Corrected import for EventsCreationForm
-
-
-
-// const App = () => {
-//   return (
-//     <Router>
-//       <div className="app">
-//         <h1>Virtual Party Planning</h1>
-//         <Routes>
-//           <Route path="/EventsCreationForm" element={<EventsCreationForm />} /> {/* Correct JSX syntax */}
-//         </Routes>
-//       </div>
-//     </Router>
-//   );
-// };
-
-// export default App;
-
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
