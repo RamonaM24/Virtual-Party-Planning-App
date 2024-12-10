1a2453f084c7128a140c3022f4c9611244760cb3
Billie501 <132241423+Billie501@users.noreply.github.com>
2024-12-10 14:35:15 +1300

    successfully merged

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom'; // Remove BrowserRouter import
 import { ThemeProvider, createTheme } from '@mui/material/styles';
-import Home from './pages/Home';  // Home component for the homepage
-import Login from './pages/Login';  // Login page
-import Register from './pages/Register';  // Register page
-import Profile from './pages/Profile';  // Profile page
-import Navbar from './Components/Navbar';  // Navbar component
+import Home from './pages/Home'; // Home component for the homepage
+import Login from './pages/Login'; // Login page
+import Register from './pages/Register'; // Register page
+import Profile from './pages/Profile'; // Profile page
+import Navbar from './Components/Navbar'; // Navbar component
 import EventsCreationForm from './Components/EventsCreationForm';
 import Invitation from './pages/Invitation';
 
@@ -14,19 +14,17 @@ const theme = createTheme();
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
-      <Router>
-        <div className="app">
-          <Navbar />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/profile" element={<Profile />} />
-            <Route path="/create-event" element={<EventsCreationForm />} /> {/* Corrected path */}
-            <Route path="/invitation" element={<Invitation />} />
-          </Routes>
-        </div>
-      </Router>
+      <div className="app">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/create-event" element={<EventsCreationForm />} />
+          <Route path="/invitation" element={<Invitation />} />
+        </Routes>
+      </div>
     </ThemeProvider>
   );
 };
