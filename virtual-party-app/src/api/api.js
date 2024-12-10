85ea22164b7560c2d45457d78e71f2f9b28699da
Billie501 <132241423+Billie501@users.noreply.github.com>
2024-12-10 14:08:27 +1300

    delete virtual-planning-app folder

--- virtual-party-app/src/api/api.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const api = axios.create({
-    baseURL: 'http://localhost:5000', //Base URL for the API
-    timeout: 10000, //Optional: Set a timeout for requests
-});
-
-export default api;
